828eaed82522399aeb7d9b16f47ddeea1bf07db4
0YB991IKB00010T\shic <ekherb1244@gmail.com>
2021-02-08 16:27:04 +0900

    Add tableau chart

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>파이차트</h1>
-  </div>
+  <div id="vizContainer" style="width:375px; height:900px;"></div>
+  <!-- <div ref="tableau"></div> -->
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+        url: "https://public.tableau.com/views/MYESG/MYESG?:language=ko&:display_count=y&publish=yes&:origin=viz_share_link",
+        options: {
+            hideTabs: true
+        }
+    }
+  },
+  // methods: {
+  //   initViz: function () {
+  //     let viz = new tableau.Viz(this.$refs.tableau, this.url, this.options);
+  //   }
+  // },
+  // mounted: function () {
+  //   this.initViz()
+  // }
+}
+</script>
