6887228e48c99460bf5635aa603c0f7482e99106
0YB991IKB00010T\shic <ekherb1244@gmail.com>
2021-02-15 15:13:52 +0900

    Edit Myesg.vue

--- test/src/views/Myesg.vue
+++ test/src/views/Myesg.vue
@@ -1,11 +1,76 @@
 <template>
-  <div>
-    나의 esg자산 상세
-  </div>
+  <CRow>
+    <CCol sm="6" lg="3">
+      <CWidgetDropdown color="success" header="나의 ESG 총자산 변화" text="현재 13,500,000원">
+        <template #footer>
+          <CChartLineSimple
+            pointed
+            class="mt-3 mx-3"
+            style="height:70px"
+            :data-points="[10000000, 10500000, 11000000, 12000000, 12500000, 12000000, 13500000]"
+            point-hover-background-color="success"
+            label="자산"
+            labels="months"
+          />
+        </template>
+      </CWidgetDropdown>
+      <CCard>
+        <CCardHeader>
+          <CIcon name="cil-justify-center"/> <strong> ESG등급 분포 </strong>
+        </CCardHeader>
+        <CCardBody>
+          <CChartRadarExample/>
+        </CCardBody>
+        <CCardFooter>
+          <CRow class="text-center">
+            <CCol md sm="12" class="mb-sm-2 mb-0">
+              <div class="text-muted">환경(Environment) 점수</div>
+              <strong>90점</strong>
+              <CProgress
+                class="progress-xs mt-2"
+                :precision="1"
+                color="success"
+                :value="90"
+              />
+              <br>
+            </CCol>
+            <CCol md sm="12" class="mb-sm-2 mb-0">
+              <div class="text-muted">사회(Social) 점수</div>
+              <strong>43점</strong>
+              <CProgress
+                class="progress-xs mt-2"
+                :precision="1"
+                color="info"
+                :value="43"
+              />
+              <br>
+            </CCol>
+            <CCol md sm="12" class="mb-sm-2 mb-0">
+              <div class="text-muted">지배구조(Governance) 점수</div>
+              <strong>85점</strong>
+              <CProgress
+                class="progress-xs mt-2"
+                :precision="1"
+                color="warning"
+                :value="85"
+              />
+            </CCol>
+          </CRow>
+        </CCardFooter>
+      </CCard>
+    </CCol>
+  </CRow>
 </template>
 
 <script>
+import CChartRadarExample from './charts/CChartRadarExample'
+import CChartLineSimple from './charts/CChartLineSimple'
+
 export default {
-  name: 'Myesg'
+  name: 'Myesg',
+  components: {
+    CChartRadarExample,
+    CChartLineSimple
+  }
 }
 </script>
---
+++ test/src/views/charts/CChartRadarExample.vue
@@ -0,0 +1,63 @@
+<template>
+  <CChartRadar
+    :datasets="defaultDatasets"
+    :options="defaultOptions"
+    :labels="[
+      'A+', 'A', 'B+', 'B',
+      'C', 'D'
+    ]"
+  />
+</template>
+
+<script>
+import { CChartRadar } from '@coreui/vue-chartjs'
+
+export default {
+  name: 'CChartRadarExample',
+  components: { CChartRadar },
+  computed: {
+    defaultDatasets () {
+      return [
+        {
+          label: 'E',
+          backgroundColor: 'rgba(000,153,051,0.2)',
+          borderColor: 'rgba(000,153,051,1)',
+          pointBackgroundColor: 'rgba(000,153,051,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(000,153,051,1)',
+          tooltipLabelColor: 'rgba(000,153,051,1)',
+          data: [45, 30, 90, 81, 56, 55]
+        },
+        {
+          label: 'S',
+          backgroundColor: 'rgba(000,204,204,0.2)',
+          borderColor: 'rgba(000,204,204,1)',
+          pointBackgroundColor: 'rgba(000,204,204,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(000,204,204,1)',
+          tooltipLabelColor: 'rgba(000,204,204,1)',
+          data: [28, 48, 40, 19, 80, 33]
+        },
+        {
+          label: 'G',
+          backgroundColor: 'rgba(255,255,000,0.2)',
+          borderColor: 'rgba(255,255,000,1)',
+          pointBackgroundColor: 'rgba(255,255,000,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(255,255,000,1)',
+          tooltipLabelColor: 'rgba(255,255,000,1)',
+          data: [20, 76, 40, 33, 30, 27]
+        }
+      ]
+    },
+    defaultOptions () {
+      return {
+        aspectRatio: 1.5
+      }
+    }
+  }
+}
+</script>
